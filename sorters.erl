24480517af04c829578eaed2446a8b5ed8cb38d7
littleandroide <jfragoso.perez@gmail.com>
2014-09-13 22:28:21 +0200

    ++++

--- sorters.erl
+++ sorters.erl
@@ -1,13 +1,16 @@
 -module(sorters).
 -author("Jonathan Fragoso").
 -vsn(1).
--export([quicksort/1]).
--import(io, [format/1]).
+-export([sort/2]).
+-import(io, [format/2]).
 
 %% Sorts a list of numbers using QuickSort Algorithm
-quicksort(NumbersList) -> hello(), 2 + 4.
+sort(quicksort, NumbersList) -> print_sorting_with("quicksort");
+sort(mergesort, NumbersList) -> print_sorting_with("mergesort");
+sort(insertionsort, NumbersList) -> print_sorting_with("insertionsort");
+sort(_, NumbersList) -> print_sorting_with("").
 
-hello() -> 
+print_sorting_with(SortAlgorithmName) -> 
 	%% io:format/1 is the standard function used to output text.
-	format("Hello, world!~n").
+	format("Sorting list of numbers, using ~s ~n", [SortAlgorithmName]).
 	
