c42c627e70b8aa46decb573b71aac9ea184362ec
littleandroide <jfragoso.perez@gmail.com>
2014-09-13 16:16:25 +0200

    project initialization

---
+++ sorters.erl
@@ -0,0 +1,11 @@
+-module(sorters).
+-export([quicksort/1]).
+-import(io, [format/1]).
+
+%% Sorts a list of numbers using QuickSort Algorithm
+quicksort(NumbersList) -> hello(), 2 + 4.
+
+hello() -> 
+	%% io:format/1 is the standard function used to output text.
+	format("Hello, world!~n").
+	
