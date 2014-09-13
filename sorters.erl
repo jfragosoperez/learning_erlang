878e96e1187f86575475d8f75c7e949a935ebd5b
littleandroide <jfragoso.perez@gmail.com>
2014-09-13 16:38:01 +0200

    +++

--- sorters.erl
+++ sorters.erl
@@ -1,8 +1,8 @@
 -module(sorters).
--export([quicksort/1]).
--import(io, [format/1]).
 -author("Jonathan Fragoso").
 -vsn(1).
+-export([quicksort/1]).
+-import(io, [format/1]).
 
 %% Sorts a list of numbers using QuickSort Algorithm
 quicksort(NumbersList) -> hello(), 2 + 4.
